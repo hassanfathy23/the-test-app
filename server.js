63740114815a3c69584bece3968a9eb0d6adba02
hassan <hassanfathy593@gmail.com>
2022-10-09 02:38:26 +0200

    comiited all changes to git

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/names", namesRoute);
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", ["http://localhost:3000", "https://tha-test-app.herokuapp.com/"]);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, DELETE, PATCH"
