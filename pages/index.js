a44fadc80618acb9738b253aa2770b478836aeb6
hassan <hassanfathy593@gmail.com>
2022-10-09 02:46:16 +0200

    committed

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,11 @@
 
-import { fetchNames } from '../lib/fetch-names'
 
 
 export default function Home() {
-  const names = [
-    "hassan", "mohamed"
-  ]
   return (
     <div>
-      {names && names.length > 0 ? names.map(name => (
-        <p key={name}>{name}</p>
-      )) : <p>problem occurred</p> }
+      hassan
     </div>
   )
 }
 
-export async function getStaticProps() {
-  const data = await fetchNames()
-  console.log(data)
-  const names = data.names
-
-  return {
-    props: {
-      names
-    },
-    revalidate: 10
-  }
-}
